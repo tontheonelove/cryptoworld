87607ef8bda23f3c7a63042978b2a8d29df995f8
tontheonelove <tonlove5233@gmail.com>
2017-11-30 10:11:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
-<!DOCTYPE html>
-<html>
 
-   <head>
-      <title>JavaScript Internal Script</title>
-      <base href = "https://www.tutorialspoint.com/" />
-      
- 
- 
  <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="zcoin" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
  
  
  
-</html>
